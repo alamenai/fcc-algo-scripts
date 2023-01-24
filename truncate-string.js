e6cad6b2f6137bfcd2cba614d558e2a7b9b8d636
Ala Eddine <ala.official.acc@gmail.com>
2023-01-25 00:06:40 +0100

    :heavy_check_mark: Implement truncateString function

---
+++ truncate-string.js
@@ -0,0 +1,6 @@
+function truncateString(str, num) {
+  if (num >= str.length) return str;
+  return str.substring(0, num).concat('...');
+}
+
+console.log(truncateString('A-tisket a-tasket A green and yellow basket', 8));
