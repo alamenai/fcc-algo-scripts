22918f351ceb7ad4f0b01633191f7571598c7880
Ala Eddine <ala.official.acc@gmail.com>
2023-01-25 13:31:18 +0100

    :bug: :heavy_check_mark: Fix #1

--- factorialize.js
+++ factorialize.js
@@ -1,6 +1,7 @@
 function factorialize(num) {
+  if (num < 0) throw new Error('Please, provide a positive number');
   if (num === 0 || num === 1) return 1;
   return num * factorialize(num - 1);
 }
 
-console.log(factorialize(5));
+console.log(factorialize(-5));
