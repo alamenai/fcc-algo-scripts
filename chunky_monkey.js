83726a71ed2249cdffc00587ac2b993e4e4524d3
Ala Eddine <ala.official.acc@gmail.com>
2023-01-25 11:54:14 +0100

    :heavy_check_mark: Implement chunkArrayInGroups function

---
+++ chunky_monkey.js
@@ -0,0 +1,10 @@
+function chunkArrayInGroups(arr, size, chunkedArray = []) {
+  const rest = Math.ceil(arr.length / size);
+  if (rest === 0) {
+    return chunkedArray;
+  }
+  const chunk = arr.slice(0, size);
+  const restArr = arr.slice(size, arr.length);
+  return [...chunkedArray, chunk, ...chunkArrayInGroups(restArr, size)];
+}
+console.log(chunkArrayInGroups(['a', 'b', 'c', 'd', 'e'], 2));
